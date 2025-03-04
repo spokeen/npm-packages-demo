15068bac5d53894e579d44fbd6f8391772167792
liaowenduo <634609252@qq.com>
2025-03-04 20:59:57 +0800

    feat(node-demo): web_server

---
+++ node-demo/learn/01web_server.js
@@ -0,0 +1,19 @@
+const {createServer} = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+/**
+ * req: http.IncomingMessage
+ * res: http.ServerResponse
+ */
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
+
