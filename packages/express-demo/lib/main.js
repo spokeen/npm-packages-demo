8a5071ae95057161d7b25a950c1ef53bb2fee52b
liaowenduo <634609252@qq.com>
2025-03-22 16:28:43 +0800

    feat(express-demo): 添加 start 命令

--- packages/express-demo/lib/main.js
+++ packages/express-demo/lib/main.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 9000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:8080`);
+  console.log(`Server running at http://localhost:${port}`);
 });
